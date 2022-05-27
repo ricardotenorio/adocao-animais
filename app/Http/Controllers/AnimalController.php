f75fe9cbafdbcc4d4874596e01554e6c805a694d
ricardotenorio <ricardo.heber@gmail.com>
2022-05-27 15:37:46 -0300

    Modificação dos nomes das tabelas no bd; criação da classe AnimalController.

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Animal;
+use App\Http\Requests\StoreAnimalRequest;
+use App\Http\Requests\UpdateAnimalRequest;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAnimalRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAnimalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAnimalRequest  $request
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAnimalRequest $request, Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Animal $animal)
+    {
+        //
+    }
+}
--- database/migrations/2022_05_13_190823_create_adocaos_table.php
+++ database/migrations/2022_05_13_190823_create_adocaos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('adocaos', function (Blueprint $table) {
+        Schema::create('adocoes', function (Blueprint $table) {
             $table->id();
             $table->string('status');
             $table->date('finalizada_em');
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('adocaos');
+        Schema::dropIfExists('adocoes');
     }
 };
