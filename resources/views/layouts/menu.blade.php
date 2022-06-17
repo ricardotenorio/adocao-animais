0c7bddf2cc01baeff06274d9a36724c8f50a3dc3
ricardotenorio <ricardo.heber@gmail.com>
2022-06-17 00:35:08 -0300

    criação dos layouts

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar 
+            navbar-expand-lg 
+            navbar-light 
+            bg-warning 
+            w-50 
+            mx-auto 
+            my-3 
+            rounded 
+            border
+            border-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-around" id="navbarNavDropdown">
+        <ul class="navbar-nav h4">
+            
+            <li class="nav-item px-5">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+            <li class="nav-item px-5">
+                <a class="nav-link" href="#">Cadastrar Animal</a>
+            </li>
+            <li class="nav-item px-5">
+                <a class="nav-link" href="#">Listas</a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
